dd890062d5821fde3d7123ec9f98a9c2f11b3bd2
elmuzyk <elmuzyk@gmail.com>
2017-02-08 10:41:22 +0100

    Data flow in app

---
+++ app/components/Examples.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Examples = React.createClass({
+
+    render: function () {
+
+      return (
+        <div>
+          <h1>Hello EXAMPLES!</h1>
+        </div>
+      );
+    }
+});
+
+module.exports = Examples;
